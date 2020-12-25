8b32b5734618cab91ca554568f8864851c5f124e
hoolek77 <szymon.kin@gmail.com>
2020-12-25 22:17:40 +0100

    Changed prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,18 @@
 module.exports = {
-    mode: 'development',
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-        ],
-    },
-
-    devtool: 'source-map',
-
-    devServer: {
-        contentBase: './dist',
-    },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+  },
 }
