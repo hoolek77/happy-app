9b43659aab00da6572b578aa8bcb4676d6c2d482
hoolek77 <szymon.kin@gmail.com>
2020-12-25 21:03:48 +0100

    Created configuration files for babel and webpack and configured them for basic usage.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+
+    devtool: 'source-map',
+
+    devServer: {
+        contentBase: './dist',
+    },
+}
