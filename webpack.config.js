6a478bcaf41f2c0973c10f4b5d3c98e6e7f702a3
hoolek77 <szymon.kin@gmail.com>
2020-12-26 11:53:58 +0100

    Fixed webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,26 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+          },
+        ],
+      },
     ],
   },
   devtool: 'source-map',
