28bd1905538b6a55e9ab54542ad648962fa412fe
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-12-29 17:10:07 +0100

    Add News API settings

--- src/environment.js
+++ src/environment.js
@@ -3,6 +3,11 @@
  */
 
 export const WEATHER_API = {
-    API_BASE_URL: '',
-    API_KEY: '',
+  API_BASE_URL: '',
+  API_KEY: '',
+}
+
+export const NEWS_API = {
+  API_BASE_URL: 'http://newsapi.org/v2/',
+  API_KEY: '9ed0a5c4878c4ca685484678e0f379c6',
 }
