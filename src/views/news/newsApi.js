0d75ba8a47ff4e632ad050438e3c98bc8f1792cf
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-12-29 18:17:03 +0100

    Add NewsAPI class

---
+++ src/views/news/newsApi.js
@@ -0,0 +1,24 @@
+import API from '../../utils/api'
+
+export default class NewsAPI extends API {
+  constructor(baseUrl, apiKey) {
+    super(baseUrl, apiKey)
+
+    this.topHeadlinesUrl = `${baseUrl}top-headlines`
+    this.language = 'en'
+
+    this.defaultHeaders = {
+      'X-Api-Key': apiKey,
+    }
+  }
+
+  fetchNews() {
+    let options = {
+      headers: this.defaultHeaders,
+    }
+
+    let query = `?language=${this.language}`
+
+    return super.fetch(this.topHeadlinesUrl, options, query)
+  }
+}
