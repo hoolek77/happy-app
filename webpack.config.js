a41b89a243b1e1b88cfedbbb54fc6241d48995f3
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-12-29 18:15:59 +0100

    Add babel-polyfill dependency to be able to use async/await features

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
