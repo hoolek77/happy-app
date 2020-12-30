bfc693c0397261ef690e54456bc0590ffc38c53f
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-12-30 19:31:35 +0100

    Add code to test news functionality to main index.html and index.js files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { NewsAPI, NewsController, NewsListView } from './views/'
+import { NEWS_API } from './environment'
+
+import './content/styles/global.css'
+
+const init = () => {
+  const rootElement = document.querySelector('main')
+
+  const newsApi = new NewsAPI(NEWS_API.API_BASE_URL, NEWS_API.API_KEY)
+  const newsListView = new NewsListView(rootElement)
+  const newsController = new NewsController(newsApi, newsListView)
+
+  newsController.fetchNews()
+}
+
+document.addEventListener('DOMContentLoaded', init)
