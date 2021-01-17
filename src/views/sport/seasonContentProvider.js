06ae37e7fc92e7a4b0569844d8ffe19fba5abc78
Ireneusz Bednorz <ireneuszbednorz@Ireneuszs-MacBook-Pro.local>
2021-01-17 13:26:15 +0100

    wybierz -> choose

--- src/views/sport/seasonContentProvider.js
+++ src/views/sport/seasonContentProvider.js
@@ -23,7 +23,7 @@ export class SeasonContentProvider extends ContentProvider {
       for (const season of Object.values(seasons)) {
         seasonsArray.push(season)
       }
-      seasonsArray.unshift({ name: 'Wybierz' })
+      seasonsArray.unshift({ name: 'Choose' })
 
       if (seasons) {
         this.seasonListModel.addSeason(
